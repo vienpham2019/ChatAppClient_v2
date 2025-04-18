42c63792a77d8d49eaf3655130e0a82441d022bb
vienpham2019 <vienpham2019@gmail.com>
2025-04-17 23:27:56 -0500

    a

--- src/components/EmojiPickerMenu.jsx
+++ src/components/EmojiPickerMenu.jsx
@@ -10,11 +10,17 @@ import { PopoverBtn, PopoverMenu } from "./PopOver";
 import { getUniqueNum } from "../helper";
 import Tooltip from "./Tooltip";
 import { useEmojiStore } from "../store/emojiStore";
+import { GroupedVirtuoso } from "react-virtuoso";
 const EmojiPickerMenu = ({ onEmojiSelect, onClose }) => {
   const popoverId = getUniqueNum();
-  const [activeCategory, setActiveCategory] = useState("recent");
+  const [activeCategory, setActiveCategory] = useState("Recently Used");
+  const getAllCategories = useEmojiStore((state) => state.getAllCategories);
+  const [selectedSkinTone, setSelectedSkinTone] = useState("#ffd225");
+  const [emojis, setEmojis] = useState({});
+  const [groupCounts, setGroupCounts] = useState([]);
+  const [preSumGroupCounts, setPreSumGroupCounts] = useState([]);
   const pickerRef = useRef(null);
-
+  const virtuosoRef = useRef(null);
   // Close emoji picker when clicking outside
   useEffect(() => {
     function handleClickOutside(event) {
@@ -29,83 +35,16 @@ const EmojiPickerMenu = ({ onEmojiSelect, onClose }) => {
     };
   }, [onClose]);
   const categories = [
-    { id: "recent", name: "Recently Used", icon: <FaClock /> },
-    { id: "smileys", name: "Smileys & People", icon: <BsEmojiSmileFill /> },
-    { id: "animals", name: "Animals & Nature", icon: <FaDog /> },
-    { id: "food", name: "Food & Drink", icon: <FaAppleAlt /> },
-    { id: "activities", name: "Activities", icon: <FaVolleyball /> },
-    { id: "travel", name: "Travel & Places", icon: <FaCar /> },
-    { id: "objects", name: "Objects", icon: <IoBulb /> },
-    { id: "symbols", name: "Symbols", icon: <MdOutlineEmojiSymbols /> },
-    { id: "flags", name: "Flags", icon: <ImFlag /> },
+    { name: "Recently Used", icon: <FaClock /> },
+    { name: "Smileys & People", icon: <BsEmojiSmileFill /> },
+    { name: "Animals & Nature", icon: <FaDog /> },
+    { name: "Food & Drink", icon: <FaAppleAlt /> },
+    { name: "Activity", icon: <FaVolleyball /> },
+    { name: "Travel & Places", icon: <FaCar /> },
+    { name: "Objects", icon: <IoBulb /> },
+    { name: "Symbols", icon: <MdOutlineEmojiSymbols /> },
+    { name: "Flags", icon: <ImFlag /> },
   ];
-
-  // Frequently used emojis
-  const frequentEmojis = [
-    "👍",
-    "😀",
-    "😍",
-    "🥰",
-    "😐",
-    "😢",
-    "😂",
-    "😅",
-    "😊",
-    "👎",
-    "🔥",
-    "😭",
-  ];
-
-  // Smileys & People section
-  const smileyEmojis = [
-    "😀",
-    "😃",
-    "😄",
-    "😁",
-    "😆",
-    "😅",
-    "🤣",
-    "😂",
-    "🙂",
-    "😊",
-    "😇",
-    "🥰",
-    "😍",
-    "🤩",
-    "😘",
-    "😗",
-    "☺️",
-    "😚",
-    "😙",
-    "🥲",
-    "😋",
-    "😛",
-    "😜",
-    "🤪",
-    "😝",
-    "🤑",
-    "🤗",
-    "🤭",
-    "🤫",
-    "🤔",
-    "🤐",
-    "🤨",
-    "😐",
-    "😑",
-    "😶",
-    "😏",
-    "😒",
-    "🙄",
-    "😬",
-    "🤥",
-    "😌",
-    "😔",
-    "😪",
-    "🤤",
-    "😴",
-  ];
-
-  const [selectedSkinTone, setSelectedSkinTone] = useState("#ffd225");
 
   const skinTones = {
     "#ffd225": "Default",
@@ -115,21 +54,45 @@ const EmojiPickerMenu = ({ onEmojiSelect, onClose }) => {
     "#a86637": "Medium Dark",
     "#60463a": "Dark",
   };
-  const getAllCategories = useEmojiStore((state) => state.getAllCategories);
-
   useEffect(() => {
     const fetchCategories = async () => {
       try {
         const categories = await getAllCategories();
-        console.log("Fetched:", categories);
+        const categoriesOrder = {
+          //   "Recently Used": 0,
+          "Smileys & People": 1,
+          "Animals & Nature": 2,
+          "Food & Drink": 3,
+          Activity: 4,
+          "Travel & Places": 5,
+          Objects: 6,
+          Symbols: 7,
+          Flags: 8,
+        };
+
+        // Create a map from your backend data
+        const sortedEmojis = [];
+        const initGroupCounts = [];
+        const initPreSumGroupCounts = new Array(8).fill(0);
+        categories.forEach(({ _id, emojis }) => {
+          const index = categoriesOrder[_id] - 1;
+          sortedEmojis[index] = emojis;
+          initGroupCounts[index] = Math.ceil(emojis.length / 8);
+        });
+        for (let i = 1; i < initPreSumGroupCounts.length; i++) {
+          initPreSumGroupCounts[i] =
+            initGroupCounts[i - 1] + initPreSumGroupCounts[i - 1];
+        }
+        setGroupCounts(initGroupCounts);
+        setPreSumGroupCounts(initPreSumGroupCounts);
+        setEmojis(sortedEmojis);
       } catch (err) {
         console.error("Error:", err.message);
       }
     };
 
     fetchCategories();
-  }, []);
-
+  }, [setEmojis]);
   return (
     <div
       ref={pickerRef}
@@ -138,19 +101,26 @@ const EmojiPickerMenu = ({ onEmojiSelect, onClose }) => {
     >
       {/* Category navigation */}
       <div className="flex items-center justify-evenly shadow">
-        {categories.map((category) => (
+        {categories.map((category, i) => (
           <button
-            key={category.id}
-            onClick={() => setActiveCategory(category.id)}
+            key={category.name + "-Icon"}
+            onClick={() => {
+              virtuosoRef.current?.scrollToIndex({
+                index: preSumGroupCounts[i - 1],
+                align: "start",
+                behavior: "smooth",
+              });
+              setActiveCategory(category.name);
+            }}
             className={`relative cursor-pointer px-1.5 py-3 h-full rounded-md hover:text-[var(--cl-snd-600)] ${
-              activeCategory === category.id
+              activeCategory === category.name
                 ? "text-[var(--cl-prim-400)]"
                 : "text-[var(--cl-snd-400)]"
             }`}
             title={category.name}
           >
             <span className="text-xl">{category.icon}</span>
-            {activeCategory === category.id && (
+            {activeCategory === category.name && (
               <span className="absolute bottom-0 left-0 w-full h-[2px] rounded bg-[var(--cl-prim-300)]"></span>
             )}
           </button>
@@ -201,42 +171,50 @@ const EmojiPickerMenu = ({ onEmojiSelect, onClose }) => {
       </div>
 
       {/* Emoji sections */}
-      <div className="overflow-y-auto max-h-[280px] p-2">
-        {/* Frequently used section */}
-        <div className="mb-4 relative">
-          <h3 className="sticky top-0 border text-sm font-medium text-gray-700 mb-2">
-            Frequently used
-          </h3>
-          <div className="grid grid-cols-8 gap-1">
-            {frequentEmojis.map((emoji, index) => (
-              <button
-                key={index}
-                className="text-2xl p-1 hover:bg-gray-100 rounded cursor-pointer"
-                onClick={() => onEmojiSelect(emoji)}
-              >
-                {emoji}
-              </button>
-            ))}
-          </div>
-        </div>
+      <div className="overflow-y-auto h-[280px] px-2">
+        <GroupedVirtuoso
+          ref={virtuosoRef}
+          style={{ height: "100%" }}
+          groupCounts={groupCounts}
+          rangeChanged={({ startIndex }) => {
+            if (preSumGroupCounts.includes(startIndex)) {
+              const currentIndex = preSumGroupCounts.indexOf(startIndex);
+              const currentCategory = emojis[currentIndex][0].category;
+              if (currentCategory !== activeCategory) {
+                setActiveCategory(currentCategory);
+              }
+            }
+          }}
+          groupContent={(index) => {
+            if (emojis[index][0].category !== activeCategory) {
+              //   setActiveCategory(emojis[index][0].category);
+            }
+            return (
+              <h3 className="top-0 p-2 text-sm font-medium text-[var(--cl-snd-800)] bg-white/90">
+                {emojis[index][0].category}
+              </h3>
+            );
+          }}
+          itemContent={(itemIndex, groupIndex) => {
+            let currentRow = itemIndex - preSumGroupCounts[groupIndex];
 
-        {/* Smileys & People section */}
-        <div>
-          <h3 className="text-sm font-medium text-gray-700 mb-2">
-            Smileys & People
-          </h3>
-          <div className="grid grid-cols-8 gap-1">
-            {smileyEmojis.map((emoji, index) => (
-              <button
-                key={index}
-                className="text-2xl p-1 hover:bg-gray-100 rounded cursor-pointer"
-                onClick={() => onEmojiSelect(emoji)}
-              >
-                {emoji}
-              </button>
-            ))}
-          </div>
-        </div>
+            return (
+              <div className="flex gap-[7px] px-1">
+                {emojis[groupIndex]
+                  .slice(currentRow * 8, currentRow * 8 + 8)
+                  .map((emoji, index) => (
+                    <button
+                      key={emoji.category + index}
+                      className="text-2xl py-2 px-1 hover:bg-gray-100 rounded cursor-pointer"
+                      onClick={() => onEmojiSelect(emoji.name)}
+                    >
+                      {emoji.skins[0].native}
+                    </button>
+                  ))}
+              </div>
+            );
+          }}
+        />
       </div>
       {/* Footer */}
       <div className="p-2 flex justify-between items-center text-gray-500 text-sm border-t border-[var(--cl-snd-200)]"></div>
